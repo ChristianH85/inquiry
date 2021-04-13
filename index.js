8b845cd3f55f3746e92f053a815717a0e017a882
Christian Henry <chenry5724@gmail.com>
2021-04-13 12:05:36 -0400

    inquiry

---
+++ index.js
@@ -0,0 +1,49 @@
+const axios=require("axios");
+const chalk =require('chalk')
+const inquirer =require('inquirer')
+
+// var requestUrl = 'https://api.github.com/users';
+
+// axios({
+//     url: requestUrl,
+//     method: 'GET',
+//   }).then(function (response) {
+//     console.log(chalk.green('Ajax Reponse \n-------------'));
+//     console.log(chalk.green(JSON.parse(response.data)))
+//   });
+let pizzaPlaces=['Via313', 'Spartan', 'Hoboken','Homeslice']
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'name',
+    },
+    {
+        type:'rawlist',
+        message:'Who makes the best pizza in Austin',
+        choices: pizzaPlaces,
+        name:'place'
+    },
+    {
+        type:'checkbox',
+        message:'What is an acceptable pizza topping',
+        choices:['pepperoni', 'cheese', 'pineapple','olives','bacon'],
+        name:'toppings'
+    }
+  ])
+  .then((response) =>{
+    console.log(chalk.yellow('Your username is: '),chalk.blue(response.username))
+    console.log(chalk.yellow('Your name is: '),chalk.green(response.name))
+    console.log(chalk.yellow('Your Favorite Pizza shop is: '),chalk.bgCyan(response.place))
+    response.toppings.includes('pineapple')?
+    console.log(chalk.bgRed('You are a monster')):
+    console.log(chalk.underline.bgYellow('I knew you were ok'))
+   
+    }
+  );
